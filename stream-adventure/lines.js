25bfda96527077e6f9f5eb06692646afa7e8c79e
duza <duza@tut.by>
2018-02-14 00:44:57 +0300

    #5. finished lines

--- stream-adventure/lines.js
+++ stream-adventure/lines.js
@@ -2,7 +2,7 @@ var through = require('through2');
 var stream = through(write, end);
 
 function write(buffer, encoding, next) {
-  let arrStringsChunk = buffer.toString().split(/\r?\n/g);
+  let arrStringsChunk = buffer.toString().split(/\r?\n+/g);
   arrStringsChunk = arrStringsChunk.map((str, index) => {
     let modifiedString;
     if (even) {
@@ -10,14 +10,10 @@ function write(buffer, encoding, next) {
     } else {
       modifiedString = str.toLowerCase();
     }
-    console.log('current even=', even);
-    if (arrStringsChunk.length > 1) {
-      even = !even;
-      console.log('future even=', even);
-    }
-    console.log('modified str=', modifiedString);
+    even = !even;
     return modifiedString;
   });
+  even = !even;
   this.push(arrStringsChunk.join('\n'));
   next();
 }
@@ -27,3 +23,26 @@ function end(done) {
 }
 var even = false;
 process.stdin.pipe(stream).pipe(process.stdout);
+
+//*** Correct version of solution from author
+//
+//var through = require('through2');
+//var split = require('split');
+//
+//var lineCount = 0;
+//var tr = through(function (buf, _, next) {
+//  var line = buf.toString();
+//  this.push(lineCount % 2 === 0
+//    ? line.toLowerCase() + '\n'
+//    : line.toUpperCase() + '\n'
+//  );
+//  lineCount ++;
+//  next();
+//});
+//process.stdin
+// .pipe(split())
+// .pipe(tr)
+// .pipe(process.stdout)
+//;
+//
+//
