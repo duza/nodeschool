4e6bb385f84812819965ea082e5a29cf77bc1e51
duza <duza@tut.by>
2018-02-17 17:43:59 +0300

    #1. understandig emit events in nodejs

---
+++ other/job.js
@@ -0,0 +1,10 @@
+var util = require('util')
+var Job = function Job() {
+  var job = this;
+  job.process = function() {
+    job.emit('done', { completedOn: new Date() });
+  };
+}
+
+util.inherits(Job, require('events').EventEmitter)
+module.exports = Job
